6fc94f0888d642180826c485503723855b91efd5
lanzoone30 <lanzoone.adrian30@gmail.com>
2026-08-13 23:27:43 -0300

    se integra el modo json con salida NDJSON

--- src/opensup/cli/main.cpp
+++ src/opensup/cli/main.cpp
@@ -5,6 +5,7 @@
 
 #include "opensup/pch.h"
 #include "opensup/cli/cli_parser.h"
+#include "opensup/cli/json_emitter.h"
 #include "opensup/core/interface.h"
 #include "opensup/common/logger.h"
 #include "opensup/version.h"
@@ -30,24 +31,42 @@ main(int argc, char** argv)
 
     common::logger_c::instance().set_level(common::log_level_e::info);
 
-    std::cout << "OpenSUP v" << OPENSUP_VERSION_STRING << " - HDMV PGS encoder" << std::endl;
-    std::cout << "Input:  " << config.input_path << std::endl;
-    std::cout << "Output: " << config.output_path << std::endl;
+    if (opts.json_mode) {
+        common::logger_c::instance().set_quiet(true);
+        common::logger_c::instance().set_callback(
+            [](const std::string& msg, int level) {
+                cli::emit_log_event(level, msg);
+            });
+
+        config.progress_cb = [](int percent, int epoch, int total) {
+            cli::emit_progress_event(percent, epoch, total);
+        };
+    } else {
+        std::cout << "OpenSUP v" << OPENSUP_VERSION_STRING << " - HDMV PGS encoder" << std::endl;
+        std::cout << "Input:  " << config.input_path << std::endl;
+        std::cout << "Output: " << config.output_path << std::endl;
+    }
 
     if (!config.overwrite && std::filesystem::exists(config.output_path)) {
         common::logger_c::instance().error(
             "Output file exists, use -y to overwrite");
+        if (opts.json_mode) {
+            cli::emit_done_event({false, "Output file exists", 0, 0, 0, 0});
+        }
         return 1;
     }
 
     core::bdn_render_c renderer(config);
     auto result = renderer.execute();
 
-    if (result.success) {
+    if (opts.json_mode) {
+        cli::emit_done_event(result);
+    } else if (result.success) {
         common::logger_c::instance().pass(
             "Success. Duration: " + std::to_string(static_cast<double>(result.duration_ms) / 1000.0) + "s");
-        return 0;
+    } else {
+        common::logger_c::instance().error(result.error);
     }
-    common::logger_c::instance().error(result.error);
-    return 1;
+
+    return result.success ? 0 : 1;
 }
